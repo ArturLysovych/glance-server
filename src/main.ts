a403abce2941f9f63e558b9d57d6d53f711f0630
ArturLysovych <arturlisovic@gmail.com>
2024-01-03 12:54:58 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:8080',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(5000);
 }
 bootstrap();
