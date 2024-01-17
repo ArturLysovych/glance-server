fdf31fd3b6f2649958d8a927974552c0a6d99204
ArturLysovych <arturlisovic@gmail.com>
2024-01-17 12:31:33 +0200

    Changed cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:8080',
+    origin: 'https://glance-client.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
